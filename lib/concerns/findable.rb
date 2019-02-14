2fbfb52b86a3c33e920422aea1ffc922ed984ecc
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-02-14 23:35:30 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,16 +1,15 @@
 module Concerns
+    module Findable
+    def find_by_name(name)
+      self.all.detect {|item| item.name == name}
+    end
 
-  module Findable
-  def find_by_name(name)
-    self.all.detect {|item| item.name == name}
-  end
-
-  def find_or_create_by_name(name)
-    if find_by_name(name)
-      find_by_name(name)
-    else
-      self.create(name)
+    def find_or_create_by_name(name)
+      if find_by_name(name)
+        find_by_name(name)
+      else
+        self.create(name)
+      end
     end
   end
-end
 end
